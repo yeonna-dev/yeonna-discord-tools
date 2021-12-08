3ae016b51d24916cab734c3429998558d204f250
esfox <osairis100@gmail.com>
2021-12-08 11:54:52 +0800

    Update guild roles API endpoint

--- src/routes/api/guilds/[guildId]/roles.js
+++ src/routes/api/guilds/[guildId]/roles.js
@@ -13,10 +13,28 @@ export async function get({ params })
   {
     const { data } = await discord.get(`/guilds/${guildId}/roles`);
     const sortedRoles = data.sort((a, b) => b.position - a.position);
-    return { body: sortedRoles };
+    const mappedRoles = sortedRoles.map(mapRole);
+    return { body: mappedRoles };
   }
   catch(error)
   {
     return { status: error.response.status, body: error.response.data };
   }
+}
+
+function mapRole(role)
+{
+  return {
+    id: role.id,
+    name: role.name,
+    permissions: role.permissions,
+    position: role.position,
+    color: role.color,
+    hoist: role.hoist,
+    managed: role.managed,
+    mentionable: role.mentionable,
+    icon: role.icon,
+    unicodeEmoji: role.unicode_emoji,
+    permissionsNew: role.permissions_new,
+  };
 }
